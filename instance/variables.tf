43a0a2df1fcb1fac7bfa79d1f7e2a9bf154c43aa
Carlos Pernia <carlospernia@MacBook-Pro-de-Carlos-2.local>
2020-11-22 18:04:35 -0300

    ADD: initital setup

---
+++ instance/variables.tf
@@ -0,0 +1,22 @@
+variable "ami_id" {
+  default = "ami-0dd2c1b8a7cfd07f0"
+  description = "Ami Id"
+}
+
+variable "instance_type" {
+  default = ""
+  description = ""
+}
+
+variable "tags" {
+  default = ""
+  description = ""
+}
+
+variable "sg_name" {
+
+}
+
+variable "ingress_rules" {
+  
+}
